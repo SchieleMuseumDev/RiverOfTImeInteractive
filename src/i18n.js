02cbb0948a4209a4b251aafe0a4ceaa43a232b1b
SchieleMuseumDev <schielemuseumdev@gmail.com>
2026-06-16 11:06:53 -0400

    fixing language translation backend!

--- src/i18n.js
+++ src/i18n.js
@@ -3,12 +3,20 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HTTPApi from "i18next-http-backend";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 i18next
 .use(initReactI18next)
 .use(LanguageDetector)
 .use(HTTPApi)
 .init({
     fallbackLng:"en",
+    backend: {
+      // Add your repository name right after the leading slash
+      loadPath: isProduction 
+      ? '/RiverOfTImeInteractive/locales/{{lng}}/{{ns}}.json' 
+      : '/locales/{{lng}}/{{ns}}.json',
+    },
     interpolation:{
         escapeValue: false
     },
